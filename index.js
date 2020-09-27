0d4692cc78afa3ba328a4d79589bd52e0de5a030
Brennon Loveless <brennon.loveless@gmail.com>
2020-09-27 12:13:42 -0400

    Added nodemon for local development. Added body parser for json requests

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello world');
+});
+
+app.post('/gaction/fulfillment', (req, res) => {
+    console.log(req.body);
+    res.json({ success: true });
 });
 
 app.listen(port, () => {
